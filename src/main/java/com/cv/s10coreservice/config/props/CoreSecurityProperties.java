c00ffea4de6b08d536d3e126119c7a21290cb860
ram <ram_r@live.com>
2025-04-03 19:53:15 +0400

    COMMIT 03042025

---
+++ src/main/java/com/cv/s10coreservice/config/props/CoreSecurityProperties.java
@@ -0,0 +1,27 @@
+package com.cv.s10coreservice.config.props;
+
+import com.cv.s10coreservice.dto.KeyAliasConfigDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Configuration
+@ConfigurationProperties(prefix = "security")
+@PropertySource("classpath:security.properties")
+public class CoreSecurityProperties {
+    private String keystorePath;
+    private String keystorePassword;
+    private KeyAliasConfigDto keystoreCurrentAlias;
+    private List<KeyAliasConfigDto> keystoreOldAliases;
+    private String JWTSecret;
+    private Long JWTAccessTokenExpirationMs;
+    private Long JWTRefreshTokenExpirationMs;
+}
