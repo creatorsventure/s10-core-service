6fc7e41bfa5cc986449dfd70ddef7ed14c5ee9af
ram <ram_r@live.com>
2025-05-12 09:30:44 +0400

    COMMIT 12-05-2025

--- src/main/java/com/cv/s10coreservice/util/StaticUtil.java
+++ src/main/java/com/cv/s10coreservice/util/StaticUtil.java
@@ -1,26 +1,14 @@
 package com.cv.s10coreservice.util;
 
 import com.cv.s10coreservice.constant.ApplicationConstant;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import java.security.Security;
 
 public class StaticUtil {
 
     public static boolean isSearchRequest(String searchField, String searchValue) {
         return StringUtils.hasText(searchField) && StringUtils.hasText(searchValue);
-    }
-
-    private static boolean initialized = false;
-    public static synchronized boolean registerBouncyCastle() {
-        if (!initialized && Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
-            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-            initialized = true;
-        }
-        return initialized;
     }
 
     public static String extractHeader(String header) throws Exception {
